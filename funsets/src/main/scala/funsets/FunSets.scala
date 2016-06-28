c10a2696dadb69f12c2e9411a49e44fdcdfb7128
sean violante <sean.violante@gmail.com>
2016-06-28 12:46:26 +0200

    first attempt at homework

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -20,7 +20,9 @@ object FunSets {
    * Returns the set of the one given element.
    */
     def singletonSet(elem: Int): Set = (i: Int) => (i==elem)
-  
+	
+  /** empty set **/
+	def emptySet(): Set = (i: Int) => False
 
   /**
    * Returns the union of the two given sets,
@@ -32,18 +34,18 @@ object FunSets {
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = (i: Int) => s(i) && t(i)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = (i: Int) => s(i) && !t(i)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = (i: Int) => s(i) && p(i)
   
 
   /**
@@ -54,25 +56,32 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a==1001) True
+      else if (!s(a)) False
+      else iter(a+1)
     }
-    iter(???)
+    iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean =  !(forall(s, (i: Int) => !p(i)))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = 
+	def iter(a: Int, s1: Set): Set = {
+      if (a==1001) s1
+      else if contains (s, i) iter(a+1, union(s1, singletonSet(f(i)) ))
+      else iter(a+1, s1)
+    }
+    iter(-bound, emptySet)
+
   
   /**
    * Displays the contents of a set
